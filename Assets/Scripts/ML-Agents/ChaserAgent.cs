985b668df8444a49081c74be92b1a8f1bf3883fc
PiCoTTo <danielgme@hotmail.com>
2024-01-21 21:33:52 +0100

    PEC3 Activity 2 - Added agent and target. Setup training configuration. Added scripts and brains resulting of two training attempts.

--- Assets/Scripts/ML-Agents/ChaserAgent.cs
+++ Assets/Scripts/ML-Agents/ChaserAgent.cs
@@ -8,11 +8,13 @@ public class ChaserAgent : Agent
     public float forceMultiplier = 10;
     Rigidbody rBody;
     Vector3 initialPosition;
+    int inTrigger = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
+        // Saves the initial position
         initialPosition = transform.localPosition;
     }
 
@@ -27,11 +29,6 @@ public class ChaserAgent : Agent
             this.transform.localPosition = initialPosition;
         }
 
-        // Move the target to a new spot
-        //Target.localPosition = new Vector3(Random.value * 50,
-        //                                   0.5f,
-        //                                   Random.value * 50);
-
         Vector3 navmeshPos;
         // Get a new random location on the NavMesh
         Utils.RandomPointOnNavMesh(new Vector3(
@@ -39,14 +36,14 @@ public class ChaserAgent : Agent
             0.0f,
             Random.value * 50), 2,
             out navmeshPos);
+
         Target.localPosition = navmeshPos;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Target and Agent positions
-        sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(Target.position);
+        sensor.AddObservation(this.transform.position);
 
         // Agent velocity
         sensor.AddObservation(rBody.velocity.x);
@@ -55,11 +52,8 @@ public class ChaserAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        // Actions, size = 2
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * forceMultiplier);
+        MoveAgent(actionBuffers.DiscreteActions);
+        SetReward(-1f / MaxStep);
 
         if (this.transform.localPosition.y < 0)
         {
@@ -67,27 +61,88 @@ public class ChaserAgent : Agent
         }
     }
 
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var direction = Vector3.zero;
+        var rotation = Vector3.zero;
+
+        var action = act[0];
+
+        switch (action)
+        {
+            case 1:
+                direction = transform.forward * 1f;
+                break;
+            case 2:
+                direction = transform.forward * -1f;
+                break;
+            case 3:
+                rotation = transform.up * 1f;
+                break;
+            case 4:
+                rotation = transform.up * -1f;
+                break;
+            case 5:
+                direction = transform.right * -0.75f;
+                break;
+            case 6:
+                direction = transform.right * 0.75f;
+                break;
+        }
+
+        transform.Rotate(rotation, Time.fixedDeltaTime * 200f);
+        rBody.AddForce(direction * 1, ForceMode.VelocityChange);
+    }
+
+
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = 0;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Prevents triggers from being permanent called
+        inTrigger++;
         if (other.gameObject.name == "Target")
         {
-            // Rewards
-            //float distanceToTarget = Vector3.Distance(this.transform.localPosition, other.gameObject.transform.localPosition);
-
             SetReward(1.0f);
             EndEpisode();
+        }
+        else if(other.transform.parent.tag == "Obstacle")
+        {
+            SetReward(-1.0f);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
+        // Only for the event that the target spawns over the agent
+        if (inTrigger > 0)
+            return;
         OnTriggerEnter(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger--;
     }
 }
